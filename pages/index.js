a6094743ee24d9be3b491765b940317b1b9503cd
pexl dev <pexldev@gmail.com>
2025-11-19 17:55:18 +0530

    Auto-update: 2025-11-19 17:55:18

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import dynamic from 'next/dynamic';
+import { Analytics } from '@vercel/analytics/react';
 
 const ResumeGenerator = dynamic(() => import('@/components/Resumegenerator'), {
   ssr: false
